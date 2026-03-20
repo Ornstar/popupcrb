c9ba1fbb8199b44810eef9fcdb8c80958c583ffa
bot <manaakumana51@gmail.com>
2026-03-21 01:01:24 +0700

    Update image URLs in createPopup function

--- popupcrb.js
+++ popupcrb.js
@@ -3,8 +3,8 @@
 
   function createPopup() {
     const IMAGES = [
-      "https://plcl.me/images/wmkVz.png",
-      "https://plcl.me/images/UwhAr.png"
+      "https://plcl.me/images/bNAKD.png",
+      "https://plcl.me/images/wmkVz.png"
     ];
 
     const BG_IMAGE = "https://plcl.me/images/wap3j.jpg";
